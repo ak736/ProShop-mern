44ac75e1432ea882980d5ba7df1312abf9f495c4
ak736 <as0700@srmist.edu.in>
2023-03-13 23:35:05 +0530

    connected to the MONGODB database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import products from './data/products.js'
 import dotenv from 'dotenv'
-dotenv.config()
+import connectDB from './config/db.js'
+import products from './data/products.js'
+import colors from 'colors'
 
+dotenv.config()
+connectDB()
 
 const app = express()
 
@@ -21,5 +24,7 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5001
 
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+)
