e1837e0bf4edc438d3afd963e8b5c9969da9e55a
ak736 <as0700@srmist.edu.in>
2023-03-24 19:37:11 +0530

    updated user and product services

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,12 @@ import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import './index.css'
 const App = () => {
   return (
     <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} />
