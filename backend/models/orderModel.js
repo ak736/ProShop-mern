3da10e029d057d6b8cbfa5e24ef7f776da38139f
ak736 <as0700@srmist.edu.in>
2023-04-28 23:27:11 +0530

    updated order models

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose'
 
 const orderSchema = mongoose.Schema(
-  
   {
     user: {
       type: mongoose.Schema.Types.ObjectID,
@@ -22,55 +21,55 @@ const orderSchema = mongoose.Schema(
       },
     ],
     shippingAddress: {
-      address: {type: String, required: true},
-      city: {type: String, required: true},
-      postalCode: {type: String, required: true},
-      country: {type: String, required: true}
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
     },
     paymentMethod: {
       type: String,
       required: true,
     },
     paymentResult: {
-      id: { type: String},
-      status: { type: String},
-      update_time: { type: String},
-      email_Address: { type: String}
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_Address: { type: String },
     },
     taxPrice: {
       type: Number,
       required: true,
-      default: 0.0
+      default: 0.0,
     },
     shippingPrice: {
       type: Number,
       required: true,
-      default: 0.0
+      default: 0.0,
     },
     totalPrice: {
       type: Number,
       required: true,
-      default: 0.0
+      default: 0.0,
     },
     isPaid: {
       type: Boolean,
       required: true,
-      default: false
+      default: false,
     },
     paidAt: {
-      type: Date
+      type: Date,
     },
     isDelivered: {
-        type: Boolean,
-        required: true,
-        default: false
+      type: Boolean,
+      required: true,
+      default: false,
     },
     deliveredAt: {
-        type: Date
+      type: Date,
     },
   },
   {
-    timeStamps: true,
+    timestamps: true,
   }
 )
 
