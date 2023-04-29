032926ad382cc9a79bf6cd826f0f53a96ca5c3c2
ak736 <as0700@srmist.edu.in>
2023-04-30 01:03:41 +0530

    added user list screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceorderScreen from './screens/PlaceorderScreen'
 import OrderScreen from './screens/OrderScreen'
+import UserListScreen from './screens/UserListScreen'
 
 const App = () => {
   return (
@@ -31,6 +32,7 @@ const App = () => {
             <Route path='/profile' element={<ProfileScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/cart/:id?' element={<CartScreen />} />
+            <Route path='/admin/userlist' element={<UserListScreen />} />
             <Route path='/' element={<HomeScreen />} />
           </Routes>
         </Container>
